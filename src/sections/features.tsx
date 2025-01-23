74406b40c44c49e71492d433f26f77a7d83a7ae5
Akhil Kumar <akhilpandu0623@gmail.com>
2025-01-23 22:36:53 +0530

    changeed name

--- src/sections/features.tsx
+++ src/sections/features.tsx
@@ -6,7 +6,7 @@ const FeaturesSection = () => {
 	return (
 		<section className="container mx-auto px-6 py-8">
 			<h2 className="text-3xl mx-auto w-full text-center font-bold text-gray-800 mb-8">
-				Why sell your house with Housing.com?
+				Why sell your house with ACREDGE?
 			</h2>
 			<div className="grid grid-cols-1 md:grid-cols-3 gap-8">
 				<AnimatedCard
